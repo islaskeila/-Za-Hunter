de9244cdae0d7b9d1818807ace4ed3d3045cbbbc
Keila Islas <kislas@bsd220.org>
2024-02-28 09:38:10 -0600

    Added map search feature

--- 'Za Hunter/ContentView.swift
+++ 'Za Hunter/ContentView.swift
@@ -19,13 +19,35 @@ struct ContentView: View {
     )
     @StateObject var locationManager = LocationManager()
     @State private var userTrackingMode: MapUserTrackingMode = .follow
+    @State private var places = [Place]()
     var body: some View {
         Map(
             coordinateRegion: $region,
             interactionModes: .all,
             showsUserLocation: true,
-            userTrackingMode: $userTrackingMode
-        )
+            userTrackingMode: $userTrackingMode,
+            annotationItems: places) { place in
+                MapMarker(coordinate: place.annotation.coordinate)
+            }
+            .onAppear(perform: {
+                performSearch(item: "Pizza")
+            })
+    }
+    func performSearch(item: String) {
+        let searchRequest = MKLocalSearch.Request()
+        searchRequest.naturalLanguageQuery = item
+        searchRequest.region = region
+        let search = MKLocalSearch(request: searchRequest)
+        search.start { (response, error )in
+            if let response = response {
+                for mapItem in response.mapItems {
+                    let annotation = MKPointAnnotation()
+                    annotation.coordinate = mapItem.placemark.coordinate
+                    annotation.title = mapItem.name
+                    places.append(Place(annotation: annotation, mapItem: mapItem))
+                }
+            }
+        }
     }
 }
 
@@ -34,3 +56,9 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+struct Place: Identifiable {
+    let id = UUID()
+    let annotation: MKPointAnnotation
+    let mapItem: MKMapItem
+}
+
